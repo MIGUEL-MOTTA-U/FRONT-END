e3dd152743cd96944a2a9c80bf042256950bcd48
MIGUEL-MOTTA-U <miguelangelmu2016@gmail.com>
2024-07-29 11:28:18 -0500

    Agregando documentacion a cart.js y diseño prelimiar de clases

--- code/js/cart.js
+++ code/js/cart.js
@@ -1,9 +1,3 @@
-//let cart = [];
-//const deliveryCost = 50; // Aqui esta en dolares todo
-/*
-Ejemplo de productos con sus caracteristicas
-*/
-
 let ShoppingCart = {
     cart: [],
     deliveryCost: 50,
@@ -19,134 +13,176 @@ let ShoppingCart = {
     {id: 8, name: "Falda con vuelo", price: 39.99, imag: "../assets/falda.webp", refCode: "00000-007",color: "Rojo", size:"S" , link: "../collections.html"},
     {id: 9, name: "Nike Air Force", price: 500.000, imag: "../assets/product/product_img_1.png", refCode: "00000-008",color: "Blanco", size:"38" , link: "../collections.html"},
     {id: 10, name: "Nike Air Force", price: 500.000, imag: "../assets/product/product_color_2.png", refCode: "00000-009",color: "Negro", size:"36" , link: "../collections.html"}
-],
+    ],
 
-getIdByName: function(name) {
-    let product = this.products.find(p => p.name === name);
-    return product ? product.id : null;
-},
+    /**
+     * Get the Id by the name of the product.
+     * 
+     * @param {string} name of the product.
+     * @returns Int, the number of id if it's found, null otherwise.
+     */
+    getIdByName: function(name) {
+        let product = this.products.find(p => p.name === name);
+        return product ? product.id : null;
+    },
 
-addToCart: function(id) {
-    let product = this.products.find(p => p.id === id);
-    if (product) {
-        let existingItem = this.cart.find(item => item.id === id);
-        if (existingItem) {
-            existingItem.quantity++;
-        } else {
-            this.cart.push({...product, quantity: 1});
-        }
-        this.updateCart();
-        this.updateCartIcon();
-        this.saveCart();
-    }
-},
-
-updateCart: function() {
-    let cartList = document.getElementById('cart-list');
-    let subtotal = document.getElementById('subtotal');
-    let deliCost = document.getElementById('deliCost');
-    let total = document.getElementById('total');
-    cartList.innerHTML = '';
-    
-    let sum = 0;
-    for(let item of this.cart) {
-        let li = document.createElement('li');
-        li.innerHTML = `
-            <div class="cart__product">
-                <img src=${item.imag} alt=${item.name}>
-                <div class="cart__item__left__info">
-                    <div class="cart__row__top">
-                        <div class="cart__item__text__top">${item.name}</div>
-                        <div class="cart__item__text__bottom">Ref: ${item.refCode}</div>
-                    </div>
-                    <div class="cart__row__bottom">
-                        <div class="cart__item__text__top">${item.size}, ${item.color}</div>
-                        <div class="cart__item__text__bottom">Cantidad: ${item.quantity}</div>
-                    </div>
-                </div>
-                <div class="cart__item__right__info">
-                    <h5>${(item.price * item.quantity).toFixed(2)} COP</h5>
-                    <button onclick="ShoppingCart.removeFromCart(${item.id})">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                </div>
-            </div>    
-        `;
-        cartList.appendChild(li);
-        sum += item.price * item.quantity;
-    }
-    
-    subtotal.textContent = sum.toFixed(2);
-    let ammountExtra = this.cart.length > 0 ? this.deliveryCost : 0;
-    deliCost.textContent = ammountExtra;
-    total.textContent = (sum + ammountExtra).toFixed(2);
-    this.saveCart();
-},
-
-updateCartIcon: function() {
-    let cartCount = document.getElementById('cart-count');
-    let subtotalItems = this.cart.reduce((sum, item) => sum + item.quantity, 0);
-    cartCount.textContent = subtotalItems;
-},
-
-saveCart: function() {
-    localStorage.setItem('cart', JSON.stringify(this.cart));
-},
-
-loadCart: function() {
-    let savedCart = localStorage.getItem('cart');
-    if (savedCart) {
-        this.cart = JSON.parse(savedCart);
-        this.cart.forEach(item => {
-            if (!item.hasOwnProperty('quantity')) {
-                item.quantity = 1;
+    /**
+     * Adds a product to the cart by the given id.
+     * 
+     * @param {int} id the id of the product to add.
+     */
+    addToCart: function(id) {
+        let product = this.products.find(p => p.id === id);
+        if (product) {
+            let existingItem = this.cart.find(item => item.id === id);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                this.cart.push({...product, quantity: 1});
             }
-        });
-    }
-},
-
-clearCart: function() {
-    let flag = this.cart.length > 0;
-    this.cart = [];
-    localStorage.removeItem('cart');
-    this.updateCart();
-    this.updateCartIcon();
-    flag ? showMessage('correct', 'Se realizó la compra con éxito.') : showMessage('error', 'El carrito está vacío.');
-},
-
-removeFromCart: function(id) {
-    let index = this.cart.findIndex(item => item.id === id);
-    if (index !== -1) {
-        if (this.cart[index].quantity > 1) {
-            this.cart[index].quantity--;
-        } else {
-            this.cart.splice(index, 1);
+            this.updateCart();
+            this.updateCartIcon();
+            this.saveCart();
         }
+    },
+
+    /**
+     * Updates the cart values and the html structure.
+     */
+    updateCart: function() {
+        let cartList = document.getElementById('cart-list');
+        let subtotal = document.getElementById('subtotal');
+        let deliCost = document.getElementById('deliCost');
+        let total = document.getElementById('total');
+        cartList.innerHTML = '';
+        
+        let sum = 0;
+        for(let item of this.cart) {
+            let li = document.createElement('li');
+            li.innerHTML = `
+                <div class="cart__product">
+                    <img src=${item.imag} alt=${item.name}>
+                    <div class="cart__item__left__info">
+                        <div class="cart__row__top">
+                            <div class="cart__item__text__top">${item.name}</div>
+                            <div class="cart__item__text__bottom">Ref: ${item.refCode}</div>
+                        </div>
+                        <div class="cart__row__bottom">
+                            <div class="cart__item__text__top">${item.size}, ${item.color}</div>
+                            <div class="cart__item__text__bottom">Cantidad: ${item.quantity}</div>
+                        </div>
+                    </div>
+                    <div class="cart__item__right__info">
+                        <h5>${(item.price * item.quantity).toFixed(2)} COP</h5>
+                        <button onclick="ShoppingCart.removeFromCart(${item.id})">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </div>
+                </div>    
+            `;
+            cartList.appendChild(li);
+            sum += item.price * item.quantity;
+        }
+        
+        subtotal.textContent = sum.toFixed(2);
+        let ammountExtra = this.cart.length > 0 ? this.deliveryCost : 0;
+        deliCost.textContent = ammountExtra;
+        total.textContent = (sum + ammountExtra).toFixed(2);
+        this.saveCart();
+    },
+
+    /**
+     * Update the cart icon according to
+     * the quantity of products in the cart.
+     */
+    updateCartIcon: function() {
+        let cartCount = document.getElementById('cart-count');
+        let subtotalItems = this.cart.reduce((sum, item) => sum + item.quantity, 0);
+        cartCount.textContent = subtotalItems;
+    },
+
+    /**
+     * Saves the cart object in the local storage.
+     */
+    saveCart: function() {
+        localStorage.setItem('cart', JSON.stringify(this.cart));
+    },
+
+    /**
+     * Loads the cart object from the local storage.
+     */
+    loadCart: function() {
+        let savedCart = localStorage.getItem('cart');
+        if (savedCart) {
+            this.cart = JSON.parse(savedCart);
+            this.cart.forEach(item => {
+                if (!item.hasOwnProperty('quantity')) {
+                    item.quantity = 1;
+                }
+            });
+        }
+    },
+
+    /**
+     * Clears the information of the cart to start another from scratch.
+     */
+    clearCart: function() {
+        let flag = this.cart.length > 0;
+        this.cart = [];
+        localStorage.removeItem('cart');
         this.updateCart();
         this.updateCartIcon();
-        this.saveCart();
-    }
-},
+        flag ? showMessage('correct', 'Se realizó la compra con éxito.') : showMessage('error', 'El carrito está vacío.');
+    },
 
-toggleCart: function() {
-    const slidingCart = document.getElementById('sliding-cart');
-    const body = document.body;
-    
-    this.isCartOpen = !this.isCartOpen;
-    
-    if (this.isCartOpen) {
-        slidingCart.classList.add('open');
-        body.classList.add('cart-open');
-    } else {
-        slidingCart.classList.remove('open');
-        body.classList.remove('cart-open');
+    /**
+     * Removes a product of the cart by the given id.
+     * 
+     * @param {int} id the id of the product
+     */
+    removeFromCart: function(id) {
+        let index = this.cart.findIndex(item => item.id === id);
+        if (index !== -1) {
+            if (this.cart[index].quantity > 1) {
+                this.cart[index].quantity--;
+            } else {
+                this.cart.splice(index, 1);
+            }
+            this.updateCart();
+            this.updateCartIcon();
+            this.saveCart();
+        }
+    },
+
+    /**
+     * Changes the html visibility of the element sliding-cart
+     * to interact with the user when the cart-icon is clicked.
+     */
+    toggleCart: function() {
+        const slidingCart = document.getElementById('sliding-cart');
+        const body = document.body;
+        
+        this.isCartOpen = !this.isCartOpen;
+        
+        if (this.isCartOpen) {
+            slidingCart.classList.add('open');
+            body.classList.add('cart-open');
+        } else {
+            slidingCart.classList.remove('open');
+            body.classList.remove('cart-open');
+        }
+        
+        this.updateCart();
     }
-    
-    this.updateCart();
-}
 };
 
-
+/**
+ * Launches a message when the user clicks the 'Finalizar Compra' 
+ * button and shows the respective Message, it deppends of the cart, if it's empty or not.
+ * 
+ * @param {string} type the type of message, it might be correct or incorrect.
+ * @param {string} text the content of the message to show in the screen.
+ */
 function showMessage(type, text) {
     const message = $('<div></div>').attr('class', `message message--${type}`);
     const icon = $('<i></i>').attr('class', type === 'correct' ? 'fa-solid fa-circle-check' : 'fa-solid fa-circle-exclamation');
